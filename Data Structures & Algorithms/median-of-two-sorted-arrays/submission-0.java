ef62045e54bb6d3bda90da719c651bd870a2496a
ParthaRoy10 <partha103096@gmail.com>
2026-06-22 13:31:40 +0530

    Add: median-of-two-sorted-arrays - submission-0

---
+++ Data Structures & Algorithms/median-of-two-sorted-arrays/submission-0.java
@@ -0,0 +1,37 @@
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int n = nums1.length;
+        int m = nums2.length;
+
+        if(n>m){return findMedianSortedArrays(nums2,nums1);}
+
+        int l = 0;
+        int r = n;
+        int mid = (n+m+1)/2;
+
+        while(l<=r){
+            int m1 = l+(r-l)/2;
+            int m2 = mid - m1;
+
+            int l1 = (m1 ==0)?Integer.MIN_VALUE:nums1[m1-1];
+            int r1 = (m1==n)?Integer.MAX_VALUE:nums1[m1];
+
+            int l2 = (m2 == 0)?Integer.MIN_VALUE:nums2[m2-1];
+            int r2 = (m2 == m)?Integer.MAX_VALUE:nums2[m2];
+
+            if(l1<=r2 && l2 <=r1){
+                if((n+m)%2 == 1){
+                    return Math.max(l1,l2);
+                }else{
+                    return ((Math.max(l1,l2)+Math.min(r1,r2))/2.0);
+                }
+            }else if(l1>r2){
+                r = m1 - 1;
+            }else{
+                l = m1+1;
+            }
+        }
+        return 0;
+
+    }
+}
