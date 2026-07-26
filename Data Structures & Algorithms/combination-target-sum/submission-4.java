3aa8c5b16dc84b4e512a99ee9091df5af350ac87
ParthaRoy10 <partha103096@gmail.com>
2026-07-26 23:26:57 +0530

    Add: combination-target-sum - submission-4

---
+++ Data Structures & Algorithms/combination-target-sum/submission-4.java
@@ -0,0 +1,29 @@
+class Solution {
+    public List<List<Integer>> combinationSum(int[] nums, int target) {
+        Arrays.sort(nums);
+        List<Integer> curr=new ArrayList<>();
+        return findSum(0,target,nums,curr);
+    }
+
+    private List<List<Integer>> res = new ArrayList<>();
+    private List<List<Integer>> findSum(int st,int need,int[] nums,List<Integer> curr){
+        
+        if(need == 0){
+            res.add(new ArrayList<>(curr));
+            return res;
+        }
+        if(need < 0){
+            return res;
+        }
+        for(int i= st;i<nums.length;i++){
+            if (need - nums[i] < 0) {
+                break; 
+            }
+            curr.add(nums[i]);
+            findSum(i,need-nums[i],nums,curr);
+            curr.remove(curr.size()-1);
+
+        }
+        return res;
+    }
+}
