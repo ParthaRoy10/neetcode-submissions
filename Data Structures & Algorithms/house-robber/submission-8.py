3d2996a7f0677d3539d439f0df11a91248d88f11
ParthaRoy10 <partha103096@gmail.com>
2026-08-16 19:26:52 +0530

    Add: house-robber - submission-8

---
+++ Data Structures & Algorithms/house-robber/submission-8.py
@@ -0,0 +1,13 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        dp = [-1] * len(nums)
+        
+        return max(self.findMax(nums,0,dp),self.findMax(nums,1,dp))
+
+    def findMax(self,nums,index,dp):
+        if index >= len(dp):
+            return 0
+        if dp[index] is not -1 :
+            return dp[index]
+        dp[index] = nums[index] + max(self.findMax(nums,index+2,dp), self.findMax(nums,index+3,dp))
+        return dp[index]
